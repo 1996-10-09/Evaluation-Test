0367a73a1b6825d58b3de54f593a90f91245b5dd
1996-10-09 <47972116+1996-10-09@users.noreply.github.com>
2020-08-04 18:28:54 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false
+      
+       
+    });
